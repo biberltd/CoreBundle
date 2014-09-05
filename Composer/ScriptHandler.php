807e0f15ef329db3796dd292c49d2ef3adf047c9
Said İmamoğlu <said.imamoglu@biberltd.com>
2014-09-05 14:33:22 +0300

    ScriptHandlers updated

--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -262,39 +262,39 @@ class ScriptHandler
         $rootDir = getcwd();
         $options = self::getOptions($event);
         $appDir = $options['symfony-app-dir'];
-        $kernelFile = $appDir.'/AppKernel.php';
-        /** read config.yml */
-        $config = Yaml::parse($rootDir.'/app/config/config.yml');
-        var_dump($config);
-        die;
+        $kernelFile = $appDir . '/AppKernel.php';
+
         $fs = new Filesystem();
         if (!$event->getIO()->askConfirmation('Would you like to install any BiberLtd bundles? [Y/n] ', true)) {
             return;
         }
-        $bundles = $event->getIO()->ask('Which bundles do you want to install?',null);
+        $bundles = $event->getIO()->ask('Which bundles do you want to install?', null);
         if (is_null($bundles)) {
             $event->getIO()->write('You did not type any bundle');
             return;
         }
-        $branch = $event->getIO()->ask('Please type a branch name for git submodules','project');
+        $branch = $event->getIO()->ask('Please type a branch name for git submodules', 'project');
         if ($branch == 'project') {
             $event->getIO()->write('You did not type branch name, \'project\' will be used.');
             return;
         }
         $client = new Git();
-        $arr = explode(',',$bundles);
+        $arr = explode(',', $bundles);
         if (count($arr) > 0) {
+            /** read config.yml */
+            $configFile = $rootDir . '/app/config/config.yml';
+            $configs = Yaml::parse($configFile);
             foreach ($arr as $name) {
                 $item = strtolower($name);
-                $folder = $rootDir.'/vendor/biberltd/'.$item.'/BiberLtd/Bundle/'.$name;
+                $folder = $rootDir . '/vendor/biberltd/' . $item . '/BiberLtd/Bundle/' . $name;
                 $fs->mkdir($folder);
-                $event->getIO()->write('Installing biberltd/'. $item.' bundle ...');
-                $folder = $rootDir.'/vendor/biberltd/'.$item.'/BiberLtd/Bundle/'.$name;
-                $repo = $client->clone_remote($folder,"https://github.com/biberltd/$item.git");
+                $event->getIO()->write('Installing biberltd/' . $item . ' bundle ...');
+                $folder = $rootDir . '/vendor/biberltd/' . $item . '/BiberLtd/Bundle/' . $name;
+                $repo = $client->clone_remote($folder, "https://github.com/biberltd/$item.git");
                 $repo = $client->open($rootDir);
                 $repo->run('submodule add ' . $folder);
                 $repo2 = $client->open($folder);
-                $repo2->run('checkout -b '.$branch);
+                $repo2->run('checkout -b ' . $branch);
                 $event->getIO()->write("biberltd/$item bundle installed successfully");
                 /** APP KERNEL*/
 //                $ref = '$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();';
@@ -302,7 +302,7 @@ class ScriptHandler
                 $bundleDeclaration = "\$bundles[] = new BiberLtd\\Bundle\\$name();";
                 $content = file_get_contents($kernelFile);
                 if (false === strpos($content, $bundleDeclaration)) {
-                    $updatedContent = str_replace($ref, $bundleDeclaration."\n         ".$ref, $content);
+                    $updatedContent = str_replace($ref, $bundleDeclaration . "\n         " . $ref, $content);
                     if ($content === $updatedContent) {
                         throw new \RuntimeException('Unable to patch %s.', $kernelFile);
                     }
@@ -310,17 +310,23 @@ class ScriptHandler
                 }
                 /** AUTLOAD NAMESPACES */
                 $ref = '\'Assetic\' => array($vendorDir . \'/kriswallsmith/assetic/src\'),';
-                $bundleDeclaration  = '\'BiberLtd\\\\Bundle\\\\'.$name.'\' => array($vendorDir . \'/biberltd/'.$item.'\'),';
-                $autloadFile = $rootDir.'/vendor/composer/autoload_namespaces.php';
+                $bundleDeclaration = '\'BiberLtd\\\\Bundle\\\\' . $name . '\' => array($vendorDir . \'/biberltd/' . $item . '\'),';
+                $autloadFile = $rootDir . '/vendor/composer/autoload_namespaces.php';
                 $content = file_get_contents($autloadFile);
                 if (false === strpos($content, $bundleDeclaration)) {
-                    $updatedContent = str_replace($ref, $bundleDeclaration."\n    ".$ref, $content);
+                    $updatedContent = str_replace($ref, $bundleDeclaration . "\n    " . $ref, $content);
                     if ($content === $updatedContent) {
                         throw new \RuntimeException('Unable to patch %s.', $kernelFile);
                     }
                     $fs->dumpFile($autloadFile, $updatedContent);
                 }
+                /** Adding orm records of bundles to config.yml */
+                $configs['doctrine']['orm']['entity_managers']['default']['mappings'][$name]['type'] = 'annotation';
+                $configs['doctrine']['orm']['entity_managers']['default']['mappings'][$name]['alias'] = $name;
+                $configs['doctrine']['orm']['entity_managers']['default']['mappings'][$name]['prefix'] = 'BiberLtd\\Core\\Bundle\\' . $name . '\\Entity';
+                $configs['doctrine']['orm']['entity_managers']['default']['mappings'][$name]['dir'] = "\"%kernel.root_dir%/../vendor/biberltd/$item/BiberLtd/Bundle/$name/Entity\"";
             }
+            file_put_contents($configFile, Yaml::dump($configs, 2));
         }
     }
 
