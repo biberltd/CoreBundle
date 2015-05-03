b0236eff0c5a722d7c2bdd96160f45830b24ff93
Can Berkol <can.berkol@biberltd.com>
2015-05-03 16:04:11 +0300

    Exception message is updated.

--- Exceptions/InvalidDateTimeException.php
+++ Exceptions/InvalidDateTimeException.php
@@ -4,8 +4,8 @@
  * @package		BiberLtd\Bundle\MemberManagementBundle
  *
  * @author		Can Berkol
- * @version     1.0.0
- * @date        07.08.2013
+ * @version     1.0.1
+ * @date        03.05.2015
  *
  * @copyright   Biber Ltd. (http://www.biberltd.com)
  * @license     GPL v3.0
@@ -21,13 +21,19 @@ class InvalidDateTimeException extends Services\ExceptionAdapter {
     public function __construct($kernel, $parameter = "", $code = 998001, Exception $previous = null) {
         parent::__construct(
             $kernel,
-            'The '.$parameter.' parameter value must be an instance of datetime.',
+            'The '.$parameter.' parameter value must be an instance of \DateTime.',
             $code,
             $previous);
     }
 }
 /**
  * Change Log:
+ * **************************************
+ * v1.0.1                      03.05.2015
+ * Can Berkol
+ * **************************************
+ * CR :: Exception message updated.
+ *
  * **************************************
  * v1.0.0                      Can Berkol
  * 07.08.2013
